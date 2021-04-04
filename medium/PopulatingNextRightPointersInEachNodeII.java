45eea081ecc450fd9b6d3232c3a1ca42840d0b8b
FengxiangLan-study <fengxianglan.study@gmail.com>
2021-04-03 22:33:37 -0700

    117. Populating Next Right Pointers in Each Node II

---
+++ medium/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,102 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (i < size - 1) {
+                    cur.next = queue.peek();
+                }
+                
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return root;
+        
+    }
+}
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        
+        Node next = root.next;
+        while (next != null) {
+            if(next.left != null) {
+                next = next.left;
+                break;
+            } else if (next.right != null) {
+                next = next.right;
+                break;
+            } else {
+                next = next.next;
+            }
+        }
+        
+        if (root.right != null) {
+            root.right.next = next;
+        }
+        
+        if (root.left != null) {
+            root.left.next = root.right == null ? next : root.right;
+        }
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+}
